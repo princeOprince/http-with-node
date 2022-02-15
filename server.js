2a9639530a63f1c2f03b5c59effc8263190efc74
Prince Odia <princeodia@gmail.com>
2022-02-15 07:31:06 +0100

    Handle file upload with formidable

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const url = require('url');
 const services = require('./services');
 const jsonBody = require('body/json');
+const formidable = require('formidable');
 
 
 const server = http.createServer();
@@ -25,7 +26,36 @@ server.on('request', (request, response) => {
         console.log("-------------------------------");
         console.log(request.headers);
     }
+    //  #region USE THE JSON BODY PACKAGE
+    else if (request.method === 'POST' && parsedUrl.pathname === '/users') {
+        jsonBody(request, response, (err, body) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                services.createSpeaker(body['userName']);
+            }
+        });
+    }
+    // #endregion
+    //  #region USE THE FORMIDABLE PACKAGE
+    else if (request.method === 'POST' && parsedUrl.pathname === '/upload') {
+        const form = new formidable.IncomingForm();
+        form.parse(request, (err, fields, files) => {
+            console.log('\n fields:');
+            console.log(fields);
+            console.log('\n files:');
+            console.log(files);
+            response.statusCode = 200;
+            response.end("Success!");
+        });
+    }
+    //   #endregion
+    else {
+        fs.createReadStream("./index.html").pipe(response);
+    }
     // #region SET HEADERS AND STATUS CODES
+    /*
     else {
         // response.statusCode = 404;
         // response.setHeader('X-Powered-By', 'Node');
@@ -36,6 +66,7 @@ server.on('request', (request, response) => {
         });
         response.end();
     }
+    */
     // #endregion
 
     // #region ADD REQUEST BODY
@@ -52,19 +83,6 @@ server.on('request', (request, response) => {
     */
     //  #endregion
     // console.log(parsedUrl);
-
-    //  #region USE THE JSON BODY PACKAGE
-    /*
-    jsonBody(request, response, (err, body) => {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            services.createSpeaker(body['userName']);
-        }
-    });
-    */
-    // #endregion
 
     // response.end('This was served with https');
 });
